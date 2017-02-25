b8a12a1487b35f7162f9c92a87e15a7d2833f75f
tomekd85 <tomekdomachowski@wp.pl>
2017-02-25 21:37:14 +0100

    Working test on asyncio loop

--- src/asyncioTests/eventLoop_tests.py
+++ src/asyncioTests/eventLoop_tests.py
@@ -4,10 +4,26 @@ import asyncio
 class TestEventLoop(unittest.TestCase):
 
     def test_event_loop_start(self):
-        loop = asyncio.AbstractEventLoop()
+        def _close_loop(loop):
+            loop.stop()
+        try:
+            loop = asyncio.get_event_loop()
+            loop.call_soon(_close_loop, loop)
+            loop.run_forever()
+        finally:
+            self.assertTrue(True)
+
+    def test_event_loop_not_running_status(self):
+        loop = asyncio.get_event_loop()
+        self.assertFalse(loop.is_closed())
+
+    def test_event_loop_running_status(self):
+        def _check_running(loop):
+            self.assertTrue(loop.is_running())
+            loop.stop()
+        loop = asyncio.get_event_loop()
+        loop.call_soon(_check_running, loop)
         loop.run_forever()
-        loop.close()
-        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
