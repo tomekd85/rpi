666e962ecd1c901ee23c5924df729fb9be8646cc
tomek from pi <tomekd85@gmail.com>
2017-03-17 17:01:13 +0100

    Working version of steering robot from keyboard

--- src/SimpleSockets/rpiMover.py
+++ src/SimpleSockets/rpiMover.py
@@ -7,21 +7,30 @@ class Mover():
         self.move_duration = refresh_rate
         self.keep_going = True
         self.moves = deque()
-        self.queue_size = 10
+        self.queue_size = 5
 
     def move(self):
         while self.keep_going:
             print("Moves = {}".format(self.moves))
-            if self.queue_size > len(self.moves) > 0:
+            num_of_moves_in_queue = len(self.moves)
+
+            if self.queue_size > num_of_moves_in_queue > 1:
+                move1 = self.moves.pop()
+                move2 = self.moves.pop()
+                move = move1 if move1 == move2 else "Stop"
+            elif num_of_moves_in_queue >= self.queue_size:
                 move = self.moves.pop()
-            elif len(self.moves) > self.queue_size:
-                for count in range(5):
-                    self.moves.pop()
+                self.moves.clear()
             else:
                 move = "Stop"
 
             func = actions.actions.get(move, actions.received)
-            func()
+            func(move)
             time.sleep(self.move_duration)
 
         actions.robot.finish_work()
+
+    def stop_moving(self):
+        self.keep_going = False
+
+
