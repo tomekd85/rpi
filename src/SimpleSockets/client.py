ffdac0be3523be4c690865aa7771a0563485db0b
tomekd85 <tomekdomachowski@wp.pl>
2017-03-17 19:08:12 +0100

    Changed client ip to rpi

--- src/SimpleSockets/client.py
+++ src/SimpleSockets/client.py
@@ -2,7 +2,7 @@ import socket
 import sys
 from getch import getch
 
-HOST = '127.0.0.1'       # The remote host
+HOST = '192.168.0.6'       # The remote host
 PORT = 58888             # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
